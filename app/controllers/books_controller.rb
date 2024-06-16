e4e9df54253bc11b60564b7ee2c249a799976b4f
ami0316 <ncxsh229@yahoo.co.jp>
2024-06-16 11:31:44 +0000

    検索機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
-  
+
   def index
     @books = Book.all
     @book = Book.new
@@ -11,14 +11,14 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-    flash[:notice] = "You have created book successfully."  
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
     else
       @user = current_user
       @books = Book.all
       render :index
     end
-    
+
   end
 
   def show
@@ -30,35 +30,37 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
-  
+
   def update
     @book = Book.find(params[:id])
-    
+
   if @book.update(book_params)
-      
+
     # 4. フラッシュメッセージを定義
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id) 
+      redirect_to book_path(@book.id)
   else
       render :edit#この記述を追加
   end
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
   private
   def book_params
       params.require(:book).permit(:title, :body)
   end
-  
-  def ensure_correct_user
-    @book = Book.find(params[:id])
-    unless @book.user == current_user
-      redirect_to books_path
-    end
-  end  
+
+
 end
