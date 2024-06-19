99d08af83ee8164fc98bdb6152e65d4e529c30dc
ami0316 <ncxsh229@yahoo.co.jp>
2024-06-19 12:55:20 +0000

    非同期通信追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,15 +1,18 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    # redirect_to book_path(book)
+    @post_comment = PostComment.new
   end
   
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    comment = PostComment.find(params[:id])
+    @book = comment.book
+    comment.destroy
+    # redirect_to book_path(params[:book_id])
   end  
   
   
